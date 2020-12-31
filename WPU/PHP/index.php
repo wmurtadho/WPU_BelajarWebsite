927c5b4cc2c67d2b7041dcf4b2f23d0ba8671a1a
wmurtadho <wmurtadho@gmail.com>
2020-12-31 10:17:07 +0700

    Update CSS Layouting

---
+++ WPU/PHP/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <style media="screen">
+
+
+      .hello{
+        background-color: blue
+      }
+    </style>
+  </head>
+  <body>
+
+<div class="hello">
+
+</div>
+
+
+  </body>
+</html>
